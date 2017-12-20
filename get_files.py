05f843d8b8458fa3b2e3a2bc040fd7f79a0458ff
beth-harper <32668890+beth-harper@users.noreply.github.com>
2017-12-20 15:39:07 +0000

    Create get_files.py

---
+++ get_files.py
@@ -0,0 +1,27 @@
+def get_files(months):
+    """ Creates a compiled list of each file in months.
+
+    This function uses the list of months to open the file using the date in the filename
+    to compile a list of all the csv files for each month in the months list.
+    """
+    
+    headings = get_headings(months)
+    full_list = []
+    full_list.append(headings)  # adds headings to top of list
+    crimes_list = [] # new list for the compiled csvs (lists)
+
+    for date in months:
+        filename = 'crime/' + date + '/' + date + '-devon-and-cornwall-street.csv'
+        file = open(filename, 'r')
+        crime_loc = list(file)  # turns into list to use indices
+
+        for row in crime_loc[1:]: # skips heading as already gotten
+            row = row.split(',')
+            row = row[0:2] + row[4:7] + row[9:10] # cleans columns
+            crimes_list.append(row)
+
+        full_list = full_list + crimes_list
+        
+    file.close()
+    
+    return full_list
