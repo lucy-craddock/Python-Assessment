3155be3e9c2f1229ab5ca7913f5b490f242dc3c7
beth-harper <32668890+beth-harper@users.noreply.github.com>
2017-12-22 15:14:13 +0000

    Update produce_output.py

--- produce_output.py
+++ produce_output.py
@@ -1,3 +1,4 @@
+
 """
 This module takes a time-range, radius and postcode to find all the crimes within a
 radius of that postcode over a time-range. This is outputted in a form of a histogram
@@ -37,27 +38,42 @@ from plot_map import plot_map
 from validate_date import validate_date
 from validate_postcode import validate_postcode
 
-postcode = input("Enter a postcode:")
-radius = input("Please choose a 1, 2 or 5 mile radius:")
-start_date = input("Enter a start date in the format YYYY-MM:")
-end_date = input("Enter an end date in the format YYYY-MM:")
+def produce_output():
+    postcode = input("Enter a postcode:")
+    radius = input("Please choose a 1, 2 or 5 mile radius:")
+    start_date = input("Enter a start date in the format YYYY-MM:")
+    end_date = input("Enter an end date in the format YYYY-MM:")
 
-valid_date1 = validate_date(start_date) # check if start date is valid
-valid_date2 = validate_date(end_date) # check if end date is valid
-valid_postcode = validate_postcode(postcode) # check if postcode is valid
+    valid_date1 = validate_date(start_date) # check if start date is valid
+    valid_date2 = validate_date(end_date) # check if end date is valid
+    valid_postcode = validate_postcode(postcode) # check if postcode is valid
 
 
-MONTHS = month_list(start_date, end_date) # gets list of months for file names
-write_csv(MONTHS) # compile all the crimes into one file
+    MONTHS = month_list(start_date, end_date) # gets list of months for file names
+    write_csv(MONTHS) # compile all the crimes into one file
 
-if validatePostcode(postcode) and (valid_date1 and valid_date2):
-    try:  # catches exemption of postcode not found 
-        post_data = centre_point(postcode, 'postcodes.csv') # gets centrepoint latlong 
-        post_lat = post_data[0]
-        post_lon = post_data[1]
-        cr_list = crimes_in_radius(post_lat, post_lon, int(radius))
-        plot_map(cr_list, postcode)
-    except ValueError:
-        print("We cannot find your postcode.")
-else:
-    print("Sorry, the details you provided were invalid!") 
+    if validatePostcode(postcode) and (valid_date1 and valid_date2):
+        try:  # catches exemption of postcode not found 
+            post_data = centre_point(postcode, 'postcodes.csv') # gets centrepoint latlong 
+            post_lat = post_data[0]
+            post_lon = post_data[1]
+            cr_list = crimes_in_radius(post_lat, post_lon, int(radius))
+            plot_map(cr_list, postcode)
+        except ValueError:
+            print("We cannot find your postcode.")
+    else:
+        print("Sorry, the details you provided were invalid!") 
+    
+    quit = input("Quit? (Y/N)")
+
+    try:
+        if (quit.lower() == "y") or (quit.lower() == "yes"):
+            sys.exit()
+        elif (quit.lower() == "n") or (quit.lower() == "no"):
+            produce_output()
+    except ValueError():
+        print("Invalid input")
+
+        
+if __name__ == "__main__":
+    produce_output()
