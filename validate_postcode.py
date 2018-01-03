237ccfa5807764d708027c6ec1ed12dd6581783b
clucy10 <lucy.craddock@mcc.co.uk>
2018-01-03 11:39:00 +0000

    pylinter for validate postcode

--- validate_postcode.py
+++ validate_postcode.py
@@ -3,18 +3,18 @@ def validate_postcode(postcode):
 
     """ Checks a string to determine if it is a valid postcode. Return boolean value where
     True indicates the postcode is valid. """
-    
+
     # Inital validity set to False
     valid = False
-    
+
     # Remove spaces in string if necessary
     if " " in postcode:
         postcode = postcode.replace(" ", "")
-        
+
     # If the postcode is longer the 7 characters, or less than 5 characters in length, then it is not valid
     if (len(postcode) < 5) or (len(postcode) > 7):
         return valid
-        
+
     elif (postcode[0].isnumeric()) or (postcode[-1].isnumeric()):
         # If the postcode has a number at the start or end then it is not valid
         return valid
@@ -24,7 +24,7 @@ def validate_postcode(postcode):
             # Test format A11AA
             if (postcode[1].isnumeric() and postcode[2].isnumeric()) and (postcode[0].isalpha() and postcode[3:].isalpha()):
                 valid = True
-                
+
         elif len(postcode) == 6:
             # Test format A111AA
             if (postcode[1:3].isnumeric()) and (postcode[0].isalpha() and postcode[4:].isalpha()):
@@ -35,7 +35,7 @@ def validate_postcode(postcode):
             # Test format A1A1AA
             elif (postcode[1].isnumeric() and postcode[3].isnumeric()) and (postcode[-1].isalpha() and postcode[0:-1:2].isalpha):
                 valid = True
-                
+
         elif len(postcode) == 7:
             # Test format AA111AA
             if (postcode[2:5].isnumeric()) and (postcode[0:2].isalpha() and postcode[5:].isalpha()):
@@ -45,19 +45,18 @@ def validate_postcode(postcode):
                 valid = True
         else:
             valid = False
-            
+
     return valid
-    
-    
+
 def test_valid():
 
-    validFormat = ["A11AA", "A111AA", "AA11AA", "A1A1AA", "AA111AA", "AA1A1AA", "DT1 1AA", "DT11 7XQ", "PL25 3UH"]
-    invalidFormat = ["A1AA", "AAAAAA", "AAA1A11AA", "AA", "AA1AAAA", "AA11111", "DT1AA", "DTXQ", "4L253UH"]
-    
-    for i in range(len(validFormat)):
-        validate = validatePostcode(validFormat[i])
+    valid_format = ["A11AA", "A111AA", "AA11AA", "A1A1AA", "AA111AA", "AA1A1AA", "DT1 1AA", "DT11 7XQ", "PL25 3UH"]
+    invalid_format = ["A1AA", "AAAAAA", "AAA1A11AA", "AA", "AA1AAAA", "AA11111", "DT1AA", "DTXQ", "4L253UH"]
+
+    for i in range(len(valid_format)):
+        validate = validate_postcode(valid_format[i])
         print(validate)
-        
-    for j in range(len(invalidFormat)):
-        validate = validatePostcode(invalidFormat[j])
+
+    for j in range(len(invalid_format)):
+        validate = validate_postcode(invalid_format[j])
         print(validate)
