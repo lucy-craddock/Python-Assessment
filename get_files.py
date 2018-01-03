bc94ca88c12c6dba2208686ed22996d696519293
clucy10 <lucy.craddock@mcc.co.uk>
2018-01-03 10:50:10 +0000

    pylint and unit testing - checks whether returns an empty list

--- get_files.py
+++ get_files.py
@@ -1,4 +1,5 @@
 from get_headings import get_headings
+from check_list import empty_list
 
 def get_files(months):
     """ Creates a compiled list of each file in months.
@@ -27,3 +28,9 @@ def get_files(months):
     file.close()
 
     return full_list
+
+if __name__ == "__main__":
+    print("testing get files")
+    assert empty_list(get_files(['2016-01']))
+    assert empty_list(get_files(['2016-01', '2016-02', '2016-03', '2016-04', '2016-05', '2016-06', '2016-07']))
+    assert empty_list(get_files(['2016-06', '2016-07', '2016-08', '2016-09', '2016-10', '2016-11', '2016-12']))
