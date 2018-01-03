58530d8debafbdb05feb4fc8e99e5f5eae091a7c
clucy10 <lucy.craddock@mcc.co.uk>
2018-01-03 10:42:26 +0000

    added function docstring

--- check_list.py
+++ check_list.py
@@ -1,6 +1,9 @@
 def empty_list(list_a):
+    """
+    Checks whether list is empty for unit testing.
+    """
+
     list_b = []
+
     if list_a == list_b:
         return False
-    else:
-        return True
