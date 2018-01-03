c09c522409753882d4b6216c911465b5cc4940a4
clucy10 <lucy.craddock@mcc.co.uk>
2018-01-03 11:36:58 +0000

    pylinter for validate datecommit

--- validate_date.py
+++ validate_date.py
@@ -15,15 +15,14 @@ def validate_date(date):
     Returns a boolean value, where True indicates the date is valid, and
     the date which was tested. If the date is valid, it is returned in the format 'YYYY-MM'.
     If invalid, it is returned in its original state. """
-    
 
     # List of month numbers
-    monthsInYear = ["01","02","03","04","05","06","07","08","09","10","11","12"]
-    
+    months_in_year = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+
     # Remove spaces in string if necessary
     if " " in date:
         date = date.replace(" ", "")
-        
+
     # Remove hyphens in string if necessary
     if "-" in date:
         date = date.replace("-", "")
@@ -34,47 +33,45 @@ def validate_date(date):
         valid = False
 
     else:
-    
+
         if (len(date) == 5) or (len(date) == 6):
             month = date[4:]
             if len(month) == 1:
                 # If the user has only used a single digit to specify the month (e.g. '1' for Jan instead of '01') - add a '0' character to the front of the month string
                 month = "0" + month
 
-            if month in monthsInYear:
+            if month in months_in_year:
                 # check to see if the month string is in the array of months (if it is, the month is valid)
                 valid = True
                 # Modify the date string so that it is in the format 'YYYY-MM'
                 date = date = date[:4] + "-" + month
-                
+
             else:
                 valid = False
-                
+
         else:
             valid = False
-            
+
     return valid, date
-      
-    
-    
+
+
 def test_valid():
 
-    validFormat = ["2016-01", "201602", "2012 04", "20161", "20165", "202012"]
-    invalidFormat = ["1234", "201614", "2016/01", "2016.01", "20160", "2016012"]
-    
+    valid_format = ["2016-01", "201602", "2012 04", "20161", "20165", "202012"]
+    invalid_format = ["1234", "201614", "2016/01", "2016.01", "20160", "2016012"]
+
     print("The given date string should only contian numbers, spaces or hyphen characters.")
     print("If the string contains any other character it is invalid.")
     print()
-    
-    for i in range(len(validFormat)):
-        validate, date = validateDate(validFormat[i])
-        print("Given date:", validFormat[i], "\nReturned date:", date)
+
+    for i in range(len(valid_format)):
+        validate, date = validate_date(valid_format[i])
+        print("Given date:", valid_format[i], "\nReturned date:", date)
         print("Valid?:", validate)
         print()
-        
-        
-    for j in range(len(invalidFormat)):
-        validate, date = validateDate(invalidFormat[j])
-        print("Given date:", invalidFormat[j], "\nReturned date:", date)
+
+    for j in range(len(invalid_format)):
+        validate, date = validate_date(invalid_format[j])
+        print("Given date:", invalid_format[j], "\nReturned date:", date)
         print("Valid?", validate)
         print()
