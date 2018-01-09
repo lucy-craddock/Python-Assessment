aec9eed0c96c5739cf488e78f918373e7a549008
beth-harper <32668890+beth-harper@users.noreply.github.com>
2018-01-09 17:35:17 +0000

    Update month_list.py
    Have added functionality to create a list containing 2017 dates as well as 2016 dates.
    Will currently only work if user gives the dates in chronological order.

--- compile_csv/month_list.py
+++ compile_csv/month_list.py
@@ -1,21 +1,28 @@
 def month_list(startdate, enddate):
+
     ''' Creates a list of months starting from a given start date to a given end date.
+    Specified years must be either 2016 or 2017. Function will only work correctly if
+    dates are given in chronological order.
 
     ATTRIBUTES:
+
         months (list): an empty list is created to be filled by strings in this format:
             '2016-01', '2016-02'. This is returned. The format is important because this
             is used to read the file names in the directory.
+
     '''
 
     months = []
 
     try:
+        # the year for the start and end date must be 2016 or 2017
         if startdate[:4] == "2016" or startdate[:4] == "2017":
             year_start = startdate[:4]
-        # the year for the start and end date must be 2016 or 2017
+
         if enddate[:4] == "2016" or enddate[:4] == "2017":
             year_end = enddate[:4]
-            
+
+           
     except:
         print('Start and end dates must be either 2016 or 2017.')
 
@@ -23,37 +30,38 @@ def month_list(startdate, enddate):
         month_start = int(startdate[5:])
         month_end = int(enddate[5:])
 
-        # compare months of start and end dates
         if year_start == year_end:
             # add dates to empty list
             for month in range(month_start, month_end):
-                date = year + '-'
+                date = year_start + '-'
                 if month < 10:
                     date += '0'
                 date += str(month)
                 months.append(date)
             months.append(enddate)
-                
-         else: # if years are different
+               
+        else: # if years are different
             for month in range(month_start, 13):
-                date = year + '-'
+                date = year_start + '-'
                 if month < 10:
                     date += '0'
                 date += str(month)
                 months.append(date)
-                
-            for month in range(01, month_end):
-                date = year + '-'
+               
+            for month in range(1, month_end):
+                date = year_end + '-'
                 if month < 10:
                     date += '0'
                 date += str(month)
                 months.append(date)
-                
+            months.append(enddate)
+               
     return months
+
 
 if __name__ == '__main__':
     print('testing month list')
     assert month_list('2016-01','2016-03') == ['2016-01', '2016-02', '2016-03']
-   # print(month_list('2016-12', '2016-10'))
-   # print(month_list('2016-12', '2016-06'))
+    print(month_list('2016-10', '2016-12'))
+    print(month_list('2016-06', '2016-12'))
     print(month_list('2016-01', '2017-12'))
